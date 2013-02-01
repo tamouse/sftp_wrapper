8220b7abf0bec9fda12b3b157b350673935540b1
Tamara Temple <tamouse@gmail.com>
2013-02-01 00:24:31 -0600

    making a connection

--- lib/sftp_wrapper.rb
+++ lib/sftp_wrapper.rb
@@ -1,9 +1,17 @@
 require "sftp_wrapper/version"
+require "net/sftp"
+
 
 module SftpWrapper
   class SftpWrapper
-    def initialize(args)
-      
+    attr_reader :sftp
+    def initialize(host,user,pw)
+      begin
+        @sftp = Net::SFTP.start(host,user, password: pw)
+      rescue Net::SSH::AuthenticationFailed => e
+        STDERR.puts "Net::SFTP Authentication Failed"
+        raise e
+      end
     end
     
     
--- spec/sftp_wrapper_spec.rb
+++ spec/sftp_wrapper_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'sftp_wrapper'
+require 'net/sftp/errors'
 
 module SftpWrapper
   describe SftpWrapper do
@@ -8,6 +9,16 @@ module SftpWrapper
     end
     it "responds to #new" do
       SftpWrapper.should respond_to(:new)
+    end
+    it "raises an exception with nil parameters" do
+      expect {SftpWrapper.new(nil,nil,nil)}.to raise_error(Net::SSH::AuthenticationFailed)
+    end
+    it "creates a connection" do
+      testhost = 'localhost'
+      testuser = 'testuser'
+      testpass = 'testpass'
+      conn = SftpWrapper.new(testhost, testuser, testpass)
+      conn.should be_instance_of(SftpWrapper)
     end
   end
   
